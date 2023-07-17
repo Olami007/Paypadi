4a2760b971832db3d33d7f52b256ca573bd63606
Olami007 <oladimejibells@gmail.com>
2023-07-17 12:29:38 +0100

    dashboard updated

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 
 const page = async () => {
-  const res = await fetch("http://localhost:3000/api/user", { method: "GET" });
+  // const res = await fetch("http://localhost:3000/api/user", { method: "GET" });
 
-  const response = await res.json();
-  return (
-    <>
-      <div>{response}</div>
-    </>
-  );
+  // const response = await res.json();
+  return <>{/* <div>{response}</div> */}</>;
 };
 
 export default page;
